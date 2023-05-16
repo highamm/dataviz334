82cb04e510185bdb9e48642a8e99257717c639d5
Matt Higham <34220951+highamm@users.noreply.github.com>
2023-05-16 14:37:26 -0400

    Initial commit

---
+++ _common.R
@@ -0,0 +1,14 @@
+# example chunk options set globally
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  warning = FALSE,
+  message = FALSE,
+  fig.height = 4
+)
+
+options(
+  width = 77,
+  dplyr.print_min = 6,
+  dplyr.print_max = 6
+)
